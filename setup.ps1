40ce7026010efa3bbbc4af239c019de99d178c09
Juanjo Prada <77102159+JuanjoPrada@users.noreply.github.com>
2025-02-13 19:54:25 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -25,13 +25,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1Ldt4hVZBkqn93I5bRn3QDFyPO-l3fpg",
-    authDomain: "finapp-21fa0.firebaseapp.com",
-    projectId: "finapp-21fa0",
-    storageBucket: "finapp-21fa0.firebasestorage.app",
-    messagingSenderId: "882910558285",
-    appId: "1:882910558285:web:838e30b1b21c8ade2ec404",
-    measurementId: "G-WLC3LXN06C"
+
 };
 
 const app = initializeApp(firebaseConfig);
